91f8748b18661ff2e6727e4064d27b3d5e9cd465
Kitsune-fx <kant.cmd@gmail.com>
2021-11-04 06:44:19 +0700

    [♦Still stuck]

--- routes/employee.js
+++ routes/employee.js
@@ -1,5 +1,6 @@
 const dbCon = require("../connection")
 
+
 module.exports = {
     addEmpPage : (req,res)=>{
         dbCon.query('SELECT * FROM department' , (error, results, field)=>{
@@ -16,18 +17,32 @@ module.exports = {
     addEmp : (req,res)=>{
         const fname = req.body.fname;
         const lname = req.body.lname;
-        const age = req.body.age;
         const salary = req.body.salary;
-        const department = req.body.department;
+        const age = req.body.age;
+        const department = req.body.department_id;
 
-        dbCon.query("INSERT INTO employee (first_name, last_name, salary, age, department_id) VALUE(?,?,?,?,?)",
-        [fname, lname, salary, age, department], (error, result, field)=>{
-            if(error){
-                throw error
-            }
-            res.redirect('/')
-        })
+         dbCon.query("INSERT INTO employee (first_name, last_name, salary, age, department_id) VALUE(?,?,?,?,?)",
+         [fname, lname, salary, age, department], (error, result, field)=>{
+             if(error){
+                 throw error
+             }
+             res.redirect('/')
+         })
 
+    },
+
+    editEmpPage : (req,res)=>{
+        console.log(req.params.id)
+
+        dbCon.query('SELECT * FROM department' , (error, results, field)=>{
+            if(error) throw error
+
+            res.render('editEmp.ejs', {
+                title : "Edit Employee",
+                data : results,
+                message: ''
+            })
+            })
     }
 
 
--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
 
-
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-
 const {getIndex} = require('./routes/index')
-const {addEmp, addEmpPage} = require('./routes/employee')
+const {addEmp, addEmpPage ,editEmp , editEmpPage} = require('./routes/employee')
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
 
 app.set('views', __dirname+'/views');
 app.set('view engine', 'ejs')
 
 app.get('/', getIndex)
+
 app.get('/add', addEmpPage)
-app.post('/add', (req,res)=>{console.log(req.body.fname)})
+app.post('/add', (req,res)=> res.json(req.body)) //I'm dead here LOL
+
+app.get('/edit:id', editEmpPage);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{console.log(`Server running on port ${PORT}`)})
