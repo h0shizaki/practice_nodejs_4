8131300ca08e8d7e401eaec758d91d8d3da2b8f0
Kitsune-fx <kant.cmd@gmail.com>
2021-11-04 23:39:40 +0700

    [🍻Finished]

--- routes/employee.js
+++ routes/employee.js
@@ -32,18 +32,57 @@ module.exports = {
     },
 
     editEmpPage : (req,res)=>{
-        console.log(req.params.id)
+        let id = req.params.id
 
         dbCon.query('SELECT * FROM department' , (error, results, field)=>{
             if(error) throw error
 
-            res.render('editEmp.ejs', {
-                title : "Edit Employee",
-                data : results,
-                message: ''
-            })
-            })
-    }
+            dbCon.query('SELECT * FROM employee where id = ?',id, (err, employee)=>{
 
+                if(err) throw err
+                if(employee === undefined || employee.length == 0) res.redirect('/')
+
+                res.render('editEmp.ejs', {
+                    title : "Edit Employee",
+                    data : results,
+                    emp : employee,
+                    message: ''
+                })
+                })
+
+            })
+
+
+    },
+
+    editEmp : (req,res)=>{
+
+        console.log(req.query)
+
+        const id = req.query.id;
+        const fname = req.query.fname;
+        const lname = req.query.lname;
+        const salary = req.query.salary;
+        const age = req.query.age;
+        const department = req.query.department_id;
+
+        dbCon.query('UPDATE employee SET first_name = ? , last_name = ? , salary = ? , age = ? , department_id = ? WHERE id = ?',
+        [fname, lname, salary, age, department, id], (error, results, field)=>{
+            
+            if(error) throw error
+
+            res.redirect('/')
+        }
+        )
+    },
+
+    delEmp: (req,res)=>{
+        let id = req.params.id
+        dbCon.query('DELETE FROM employee WHERE id = ?',id ,(error, results, field)=>{
+            if(error) throw error
+
+            res.redirect('/')
+        })
+    }
 
 }
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const {getIndex} = require('./routes/index')
-const {addEmp, addEmpPage ,editEmp , editEmpPage} = require('./routes/employee')
+const {addEmp, addEmpPage ,editEmp , editEmpPage , delEmp} = require('./routes/employee')
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json())
@@ -16,6 +16,9 @@ app.get('/add', addEmpPage)
 app.get('/insert', addEmp) //I'm dead here LOL
 
 app.get('/edit:id', editEmpPage);
+app.get('/put', editEmp )
+
+app.get('/del:id', delEmp )
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{console.log(`Server running on port ${PORT}`)})
